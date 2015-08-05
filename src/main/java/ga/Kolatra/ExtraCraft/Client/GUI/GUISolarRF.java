f6a8a8cfb606f773abf134ad800cd6db51b1c366
Kolatra <t.louks88@gmail.com>
2015-08-04 19:30:20 -0600

    Create GUI class.

---
+++ src/main/java/ga/Kolatra/ExtraCraft/Client/GUI/GUISolarRF.java
@@ -0,0 +1,12 @@
+package ga.Kolatra.ExtraCraft.Client.GUI;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+
+public class GUISolarRF extends GuiContainer
+{
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_)
+    {
+
+    }
+}
