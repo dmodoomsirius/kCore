1cd9bd4a1d990bc7380230f9b27ab0b8761aa57e
Kolatra <t.louks88@gmail.com>
2015-08-11 01:38:43 -0600

    Remove BlockRandom. It's not being used and it was stupid anyways.

--- src/main/java/ga/Kolatra/ExtraCraft/Common/Block/ModBlocks.java
+++ src/main/java/ga/Kolatra/ExtraCraft/Common/Block/ModBlocks.java
@@ -1,24 +1,20 @@
 package ga.Kolatra.ExtraCraft.Common.Block;
 
 import ga.Kolatra.ExtraCraft.Common.TileEntity.TileSolarRF;
-import ga.Kolatra.ExtraCraft.Common.TileEntity.TileTest;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 
 public class ModBlocks
 {
-    public static Block blockRandom = new BlockRandom();
     public static Block blockSolarRF = new BlockSolarRF();
 
     public static void register()
     {
         // Blocks
-        GameRegistry.registerBlock(blockRandom, "random_block");
         GameRegistry.registerBlock(blockSolarRF, "solar_rf");
 
         // Tile Entities
-        GameRegistry.registerTileEntity(TileTest.class, "TestBlockTile");
         GameRegistry.registerTileEntity(TileSolarRF.class, "SolarRFTile");
     }
 }
