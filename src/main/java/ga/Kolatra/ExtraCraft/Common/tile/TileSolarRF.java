75c0fc659e03a426ebb8ea72ce75c3772f3e88e9
Kolatra <t.louks88@gmail.com>
2015-08-29 05:22:08 -0600

    The Solar RF Tile actually generates and sends power now.

--- src/main/java/ga/Kolatra/ExtraCraft/Common/tile/TileSolarRF.java
+++ src/main/java/ga/Kolatra/ExtraCraft/Common/tile/TileSolarRF.java
@@ -3,6 +3,7 @@ package ga.Kolatra.ExtraCraft.Common.Tile;
 import cofh.api.energy.EnergyStorage;
 import cofh.api.energy.IEnergyHandler;
 import cofh.api.energy.IEnergyProvider;
+import cofh.api.energy.IEnergyReceiver;
 import cofh.api.energy.TileEnergyHandler;
 
 import net.minecraft.nbt.NBTTagCompound;
@@ -10,13 +11,12 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.biome.BiomeGenDesert;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class TileSolarRF extends TileEntity implements IEnergyProvider
+public class TileSolarRF extends TileEntity implements IEnergyProvider, IEnergyReceiver
 {
     // Variables
     public static boolean isUnderSun;
     public static int energyStored;
     public int maxExtract = 100;
-    public int maxReceive = 100;
     public int maxPower = 1000000;
     public int generationRate = 10;
     protected TileEnergyHandler energy = new TileEnergyHandler();
@@ -50,7 +50,6 @@ public class TileSolarRF extends TileEntity implements IEnergyProvider
         if ((storage.getEnergyStored() > 0)) {
             for (int i = 0; i < 6; i++){
 
-                //ForgeDirection is a useful helper class for handling directions.
                 int targetX = xCoord + ForgeDirection.getOrientation(i).offsetX;
                 int targetY = yCoord + ForgeDirection.getOrientation(i).offsetY;
                 int targetZ = zCoord + ForgeDirection.getOrientation(i).offsetZ;
@@ -58,11 +57,11 @@ public class TileSolarRF extends TileEntity implements IEnergyProvider
                 TileEntity tile = worldObj.getTileEntity(targetX, targetY, targetZ);
                 if (tile instanceof IEnergyHandler) {
 
-                    int maxExtract = this.maxExtract; //Gets the maximum amount of energy that can be extracted from this tile in one tick.
-                    int maxAvailable = storage.extractEnergy(maxExtract, true); //Simulates removing "maxExtract" to find out how much energy is actually available.
-                    int energyTransferred = ((IEnergyHandler) tile).receiveEnergy(ForgeDirection.getOrientation(i).getOpposite(), maxAvailable, false); //Sends "maxAvailable" to the target tile and records how much energy was accepted.
+                    int maxExtract = this.maxExtract;
+                    int maxAvailable = storage.extractEnergy(maxExtract, true);
+                    int energyTransferred = ((IEnergyHandler) tile).receiveEnergy(ForgeDirection.getOrientation(i).getOpposite(), maxAvailable, false);
 
-                    storage.extractEnergy(energyTransferred, false); //Extract the energy transferred from the internal storage.
+                    storage.extractEnergy(energyTransferred, false);
                 }
             }
         }
@@ -125,6 +124,12 @@ public class TileSolarRF extends TileEntity implements IEnergyProvider
     public boolean canConnectEnergy(ForgeDirection from)
     {
         return from != ForgeDirection.UP;
+    }
+
+    @Override
+    public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate)
+    {
+        return storage.receiveEnergy(maxReceive, simulate);
     }
 
     @Override
